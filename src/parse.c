4940bef44eab03278632fa922979f6d5524afa5f
TEMI <temi@temi-Inspiron-1470.(none)>
2013-01-04 00:08:08 +0100

    Petites modifications

--- src/parse.c
+++ src/parse.c
@@ -67,30 +67,18 @@ int CommentaireOuvert1(char * nom){
 			}
 		}
     }
- return 0;
-}
-
-#ifdef IF
-int CommentaireOuvert2(char * nom){
-	int i; 
-	int n;
-	FILE *from; 
-	char buffer[BUFFER_SIZE];		
-	if (NULL == (from = fopen(nom ,"rb"))) {
+	
+	if (ferror(from)) {
+		perror("read"); 
+		clearerr(from); 
 		return -1;
-	}	
-	while ((n = fread(buffer,sizeof(char),BUFFER_SIZE,from)) != 0) {
-		for(i = 0; i < BUFFER_SIZE; i++){
-			if( ((buffer[i] == '/') && (buffer[i+1] == '*') && (buffer[i+2] == '*')) ){
-				/*fprintf(stdout," on a trouvé le dedut du commentaire\n "); */
-				return 1;
-			}
-		}
-    }
- return 0;
-}
+	}
+	
+	fclose(from);
+	return 0;
+ }
 
-#endif 
+
 
 int CommentaireFerme(char * nom){
 	int i; 
@@ -108,9 +96,16 @@ int CommentaireFerme(char * nom){
 			}
 		}
 	}
- return 0;
-}
-
+	
+	if (ferror(from)) {
+		perror("read"); 
+		clearerr(from); 
+		return -1;
+	}
+	
+	fclose(from);
+	return 0;
+ }
 
 
 int CommentaireTriple(char * nom){
@@ -129,8 +124,16 @@ int CommentaireTriple(char * nom){
 			}
 		}
 	}
- return 0;
-}
+	
+	if (ferror(from)) {
+		perror("read"); 
+		clearerr(from); 
+		return -1;
+	}
+	
+	fclose(from);
+	return 0;
+ }
 
 
 int CommentaireError(char * nom){
@@ -148,27 +151,27 @@ int CommentaireError(char * nom){
 			}
 		}
 	}
- return 0;
-}
 	
+	if (ferror(from)) {
+		perror("read"); 
+		clearerr(from); 
+		return -1;
+	}
+	
+	fclose(from);
+	return 0;
+ }
 	
 
 void DetecteCommentaire(char *nom){
-
-	if( (CommentaireOuvert1(nom) == 1) && (CommentaireFerme(nom) == 1) ) {
-		fprintf(stdout," on a trouvé un commentaire\n ");
-		return;
-	}
 	
-  #ifdef IF
-   else if( (CommentaireOuvert2(nom) == 1) && (CommentaireFerme(nom) == 1) ) {
-		fprintf(stdout," on a trouvé un commentaire\n de type /** ... */");
+	if( (CommentaireOuvert1(nom) == 1) && (CommentaireFerme(nom) == 1) ) {
+		fprintf(stdout," -on a trouvé un commentaire:\n ");		
 		return;
 	}
-  #endif	
   
 	else if( CommentaireTriple(nom) == 1) {
-		fprintf(stdout," on a trouvé un commentaire\n");
+		fprintf(stdout," -on a trouvé un commentaire\n");
 		return;
 	}
 	
@@ -178,27 +181,113 @@ void DetecteCommentaire(char *nom){
 	}
 			
 	else {
-		fprintf(stdout," on a pas trouvé de commentaire\n \n");
+		fprintf(stdout," -on a pas trouvé de commentaire\n \n");
 		return;
 	}
 }
 
-		
+#ifdef IF		
+void RecupererContenuCommentaire(){
+	int n;
+	int i = 0;
+	 
+	
+}
+
+int RecupereLaLigne(char * src, char * dest, int line)
+{
+    int i;
+    i = 0;
+    while(*src && i < line) {
+        if(*src++ == '\n') i++;
+    }
+    if(*src && i == line) {
+        i = 0;
+        while(*src && *src != '\n') {
+            dest[i++] = *src++; 
+        }
+        dest[i] = 0;
+    } else {
+        i = 0;
+    }
+    return i;
+}
+#endif
+
+
+int RecupereLaSignature(char * nom){
+	int i; 
+	int n;
+	FILE *from; 
+	char buffer[BUFFER_SIZE];
+
+
+	if (NULL == (from = fopen(nom ,"rb"))) {
+		return -1;
+	}
+	
+	while ((n = fread(buffer,sizeof(char),BUFFER_SIZE,from)) != 0) {
+		for(i = 0; i < n; i++){			
+			if((buffer[i] == 'v') && (buffer[i+1] == 'o') && (buffer[i+2] == 'i') && (buffer[i+3] == 'd')){	
+				fprintf(stdout," -on a trouvée un debut de signature commençant par void \n");				
+			}
+			else if((buffer[i] == 'i') && (buffer[i+1] == 'n') && (buffer[i+2] == 't')){	
+				fprintf(stdout," -on a trouvée un debut de signature commençant par int\n");	
+			}
+			else if((buffer[i] == 'f') && (buffer[i+1] == 'l') && (buffer[i+2] == 'o') && (buffer[i+3] == 'a') && (buffer[i+4] == 't')){	
+				fprintf(stdout," -on a trouvée un debut de signature commençant par float\n");	
+			}
+			else if((buffer[i] == 'd') && (buffer[i+1] == 'o') && (buffer[i+2] == 'u') && (buffer[i+3] == 'b') && (buffer[i+4] == 'l') && (buffer[i+4] == 'e')){	
+				fprintf(stdout," -on a trouvée un debut de signature commençant par double\n");	
+			}	
+			else if((buffer[i] == 'l') && (buffer[i+1] == 'o') && (buffer[i+2] == 'n') && (buffer[i+3] == 'g')){	
+				fprintf(stdout," -on a trouvée un debut de signature commençant par long\n");	
+			}										
+		 }	
+	 }
+			
+	if (ferror(from)) {
+		perror("read"); 
+		clearerr(from); 
+		return -1;
+	}
+	
+	fclose(from);
+	return 0;
+ }
+
 
 
 
 #ifdef IF
+int RecupereLaLigne(char * src, char * dest, int (*recuperer_signature)(char*)) /* <-RecupereLaSignature au moment où on l'appel */
+{
+	recuperer_signature = RecupereLaSignature;
+    int i;
+    i = 0;
+    while(*src && i < recuperer_signature) {
+        if(*src++ == '\n') i++;
+    }
+    if(*src && i == recuperer_signature) {
+        i = 0;
+        while(*src && *src != '\n') {
+            dest[i++] = *src++; 
+        }
+        dest[i] = 0;
+    } else {
+        i = 0;
+    }
+    return i;
+}
 
- 	
+#endif
+
+
+
+
 int DetecteBalise(char *nom){
 	FILE *from; 
 	char buffer[BUFFER_SIZE];
-	char buffer1[4] = "file";
-	char buffer2[6] = "author";
-	char buffer3[7] = "version";
-	char buffer4[4] = "date";
-	char buffer5[5] = "brief";
-	
 	int n;
 	int i;
 	
@@ -207,25 +296,18 @@ int DetecteBalise(char *nom){
 	}	
 
 	while ((n = fread(buffer,sizeof(char),BUFFER_SIZE,from)) != 0) {
-		for( i = 0; i <= n; i++){
-			if( (buffer[i] == '\\') && (buffer[i+1] == *buffer1) ){
-				fprintf(stdout," on a trouvé la balise fichier.c \n "); 		
+		for( i = 0; i <= n; i++){  
+			if( (buffer[i] == '\\') && (buffer[i+1] != '\n')) {
+				fprintf(stdout," -on a trouvé une balise \n "); 
 			}
-			if( (buffer[i] == '\\') && (buffer[i+1] == *buffer2) ){
-				fprintf(stdout," on a trouvé la balise auteur \n "); 		
+			else if ((buffer[i] == '\\') && (buffer[i+1] == '\n') ){
+				fprintf(stdout," Attention il ya une une balise vide \n "); 
 			}
-			if( (buffer[i] == '\\') && (buffer[i+1] == *buffer3) ){
-				fprintf(stdout," on a trouvé la balise version \n "); 		
-			}
-			if( (buffer[i] == '\\') && (buffer[i+1] == *buffer4) ){
-				fprintf(stdout," on a trouvé la balise date \n "); 		
-			}
-			if( (buffer[i] == '\\') && (buffer[i+1] == *buffer5) ){
-				fprintf(stdout," on a trouvé la balise brief \n "); 		
-			}
-		} 
+			else if ((buffer[i] == '\\') && (buffer[i+1] == ' ')){ 
+				fprintf(stdout," Attention il ya une erreur au niveau d'une balise  \n ");
+			} 
+		}
 	}
-	
 	if (ferror(from)) {
 		perror("read"); 
 		clearerr(from); 
@@ -235,5 +317,3 @@ int DetecteBalise(char *nom){
 	fclose(from);	
 	return 0; 
 }
-
-#endif
---
+++ test.c
@@ -0,0 +1,12 @@
+/**
+* \file
+* \
+* \   file 
+
+*/
+
+int fonctiontest ();
+voi fonctiontest ();
+float fonctiontest ();
+Double fonctiontest ();
+long fonctiontest ();
