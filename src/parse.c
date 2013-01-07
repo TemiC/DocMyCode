2d69bab92c6f20049e83f7009d746c9575a9c5c0
TEMI <temi@temi-Inspiron-1470.(none)>
2013-01-07 23:50:46 +0100

    petit soucis au niveau de la récupération d'une balise type

--- src/parse.c
+++ src/parse.c
@@ -7,50 +7,6 @@
 # define BUFFER_SIZE 1024
 
 
-
-#ifdef IF
-int DetecteCommentaire(char *nom){
-	FILE *from; 
-	char buffer[BUFFER_SIZE];
-	int n;
-	int i;
-	int j;
-	
-	if (NULL == (from = fopen(nom ,"rb"))) {
-		return -1;
-	}	
-	
-	while ((n = fread(buffer,sizeof(char),BUFFER_SIZE,from)) != 0) {
-		for( i = 0; i <= n; i++){
-			if( ((buffer[i] == '/') && (buffer[i+1] == '*'))  ||  ((buffer[i] == '/') && (buffer[i+1] == '*') && (buffer[i+2] == '*'))){
-				fprintf(stdout," on a trouvé le dedut du commentaire\n "); 	
-				continue;	
-			}
-			j = i+1;
-			if( (buffer[j] == '*') && (buffer[j+1] == '/') ){
-				fprintf(stdout," on a trouvé la fin du commentaire\n "); 	
-				continue;		
-			}
-			if( ((buffer[i] == '/') && (buffer[i+1] == '/')) ){
-				fprintf(stdout," on a trouvé un commentaire\n "); 	
-				continue;
-			}			
-		}
-	 } 
-	
-	if (ferror(from)) {
-		perror("read"); 
-		clearerr(from); 
-		return -1;
-	}
-	
-	fclose(from);
-	return 0;
- }
- 	
-#endif
-
-
 int CommentaireOuvert1(char * nom){
 	int i; 
 	int n;
@@ -187,12 +143,7 @@ void DetecteCommentaire(char *nom){
 }
 
 #ifdef IF		
-void RecupererContenuCommentaire(){
-	int n;
-	int i = 0;
-	 
-	
-}
+
 
 int RecupereLaLigne(char * src, char * dest, int line)
 {
@@ -215,6 +166,96 @@ int RecupereLaLigne(char * src, char * dest, int line)
 #endif
 
 
+String* Initialisation( char* mot ){
+	String* string = (String*)malloc(sizeof(String));
+
+	if( string != NULL){
+		string->length = strlen(mot);
+		string->mot = (char*)malloc((string->length+1)*sizeof(char));
+		if( string->mot != NULL){
+			string->mot = strcpy(string->mot,mot);
+			return string;
+		}
+		return NULL;
+	}
+	return NULL;
+}
+
+int RecupereUnMot(String* string,char * test){
+	char buffer[BUFFER_SIZE];
+	/*char *buffertest = "\\file";*/
+	char buffertest[BUFFER_SIZE];
+	int i;
+
+	
+	for(i = 0; i <= BUFFER_SIZE; i++){
+		if(buffer[i] != ' '){
+			buffer[i] = string->mot[i];
+			buffer[i] = test[i];
+			if(compareChaine(test,buffertest) != 0){
+				/*printf("On a trouvé la balise %s\n", &test[i]);*/
+				return -1;
+			}
+		}
+	}
+	return 0;
+}
+
+
+int RecupereBaliseFile(char * nom){ 
+	int i; 
+	int n;
+	FILE *from; 
+	char buffer[BUFFER_SIZE];
+	char *buffertest = "\\file";
+	String* tmp = Initialisation(buffer);
+
+	if (NULL == (from = fopen(nom ,"rb"))) {
+		return -1;
+	}
+	
+	while ((n = fread(buffer,sizeof(char),BUFFER_SIZE,from)) != 0){		
+		printf("test\n");
+			if(RecupereUnMot(tmp, buffertest) != -1){
+				printf("test1\n");
+				printf("On a trouvé la balise %s\n", &tmp[i]);
+			 }
+		
+	}	
+	if (ferror(from)) {
+	perror("read"); 
+	clearerr(from); 
+	return -1;
+	}
+	
+	fclose(from);
+	return 0;
+}
+
+
+int compareChaine(char *chaine1, char *chaine2){
+	while( (*chaine1) && (*chaine1 == *chaine2) ){
+		chaine1++; /* On va au caractère suivant de la première chaine */
+		chaine2++; /* On va au caractère suivant de la seconde chaine */
+	}
+	return (*chaine1 - *chaine2); /* si c'est égale 0 alors ok */
+}
+
+
+int RecupereBaliseAuteur(String * tmp){ 
+    int i; 
+    char buffertest[6] = "author";
+   
+	for(i = 0; i <= tmp->length; i++){
+		if(tmp->mot == buffertest)
+		return 1;
+	}
+	return 0; 
+}
+
+
+
+/* /!\ a re-faire */
 int RecupereLaSignature(char * nom){
 	int i; 
 	int n;
@@ -290,13 +331,14 @@ int DetecteBalise(char *nom){
 	char buffer[BUFFER_SIZE];
 	int n;
 	int i;
+
 	
 	if (NULL == (from = fopen(nom ,"rb"))) {
 		return -1;
 	}	
 
 	while ((n = fread(buffer,sizeof(char),BUFFER_SIZE,from)) != 0) {
-		for( i = 0; i <= n; i++){  
+		for( i = 0; i <= BUFFER_SIZE; i++){  
 			if( (buffer[i] == '\\') && (buffer[i+1] != '\n')) {
 				fprintf(stdout," -on a trouvé une balise \n "); 
 			}
@@ -308,6 +350,7 @@ int DetecteBalise(char *nom){
 			} 
 		}
 	}
+	
 	if (ferror(from)) {
 		perror("read"); 
 		clearerr(from); 
