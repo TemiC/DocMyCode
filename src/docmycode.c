45a7750b314ddd7230670c33126f53e8e4ad996e
TEMI <temi@temi-Inspiron-1470.(none)>
2013-01-19 18:52:27 +0100

    Modification du fichier contenant le main

--- src/docmycode.c
+++ src/docmycode.c
@@ -1,102 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-# include <stdio.h>
-# include <stdlib.h>
-# include "docmycode.h"
-# include "explorer.h"
-# include "filestream.h"
-# include "parse.h"
+#include "html.h"
+#include "file.h"
+#include "fileexplorer.h"
+#include "html.h"
 
+#define		DEBUG_MAIN		1
 
-
-
-
-
-int main (int argc, char*argv[]){
-	FILE* file;
-	int i; 
-	for(i = 1; i< argc; i++){
-		if( (file = fopen(argv[i], "rb") ) == NULL) {
-			fprintf(stderr, "Erreur ouverture fichier %s\n",argv[i]);
-			return -1;
-		}
-		fprintf(stdout, "\nYour file contain :\n");
-		printf("\n");
-		cat(argv[i]); 
-		printf("\n");
-		
-		DetecteCommentaire(argv[i]);
-		printf("\n");
-		
-		fprintf(stdout, "Balise trouvée :\n");	
-		DetecteBalise(argv[i]);
-		/*cat(argv[i]);*/ 
-		fclose(file);
-	}
- return 0; 
-}
+int main(int argc, char** argv, char** env) {
 	
-#ifdef IF
-/* utiliser getopt pour les options !!!*/
-
-
-int main(int argc, char* argv[]){
-	DIR* dir;
-	FILE* file1;
-	if(argc < 2){
-		fprintf(stderr, "ERROR\n");
-		return -1; 
-	 } 
-	 
-	 
-	/*if ( (dir = opendir(argv[1]) ) == NULL) {
-		fprintf(stderr, "Erreur ouverture fichier\n");
-		return -1;
+	if (argc < 2) {
+		fprintf(stderr, "Please type your project's location.\n\n");
+		return EXIT_FAILURE;
 	}
-
-	fprintf(stdout, "Your directory contain :\n");
-	if( readcontainsdir(argv[1]) != -1) { */
-		if( (file1 = fopen(argv[1], "rb") ) == NULL) {
-			fprintf(stderr, "Erreur ouverture fichier %s\n",argv[1]);
-			return -1;
-		}
-	/*	if((file2 = fopen("docmycode.html","w+") ) == NULL){
-			fprintf(stderr, "Erreur ouverture fichier %s\n", argv[1]);
-			return -1;
-		}*/
-		
-		fprintf(stdout, "\nYour file contain :\n");
-		printf("\n");
-		cat(argv[1]); 
-		printf("\n");
-		
-		DetecteCommentaire(argv[1]);
-		printf("\n");
-		
-		fprintf(stdout, "Balise trouvée :\n");	
-		DetecteBalise(argv[1]);
-		
-		RecupereBaliseFile(argv[1]); 
-		RecupereBaliseAuteur(argv[1]);
-		
-		
-	/*	String* tmp = Initialisation(argv[1]);
-		
-		RecupereBaliseFile(tmp); */
-		/*printf("file: %s\n",tmp->mot);
-		
-		RecupereBaliseAuteur(tmp);
-		printf("author: %s\n",tmp->mot);*/
-		printf("\n");
-		
-		fprintf(stdout, "Signature trouvée :\n");	
-		RecupereLaSignature(argv[1]);
-		printf("\n");
-		
-		fprintf(stdout, "End of file\n");
-		fclose(file1);
-  /* } */
-
- return 0; 
+	ProjectInfo* projectInfo = initProjectInfo(argv[1]);		
+	printf("%s\n", projectInfo->projectPath);
+	
+	exploreProjectDirectory(projectInfo->projectPath);
+	
+	
+	
+	
+	
+	
+	return EXIT_SUCCESS;
 }
-
-#endif
